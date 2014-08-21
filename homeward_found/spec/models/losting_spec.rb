5405f2c0cf0ea8437ede9e84a7c56093a9daab36
Gunnari Auvinen <gauvinen@gmail.com>
2014-08-20 19:22:08 -0700

    Add test to verify that Losting.ordered_json returns a maximum of 10 results.

--- homeward_found/spec/models/losting_spec.rb
+++ homeward_found/spec/models/losting_spec.rb
@@ -6,4 +6,9 @@ describe Losting do
     losting_date = losting.event_date
     expect(losting.long_date).to eq(losting_date.to_formatted_s(:long_ordinal))
   end
+
+  it 'returns a maximum of 10 event dates upon call to ordered_json' do
+    15.times { FactoryGirl.create(:losting) }
+    expect(JSON.parse(Losting.ordered_json).length).to eq(10)
+  end
 end
