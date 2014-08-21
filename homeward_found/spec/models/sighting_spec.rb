656fd79bf4b0ca9a6ef96da732bdacb8dd20de29
Gunnari Auvinen <gauvinen@gmail.com>
2014-08-20 21:07:16 -0700

    Add test to verify that Sighting.ordered_json returns a maximum of 10 records.

--- homeward_found/spec/models/sighting_spec.rb
+++ homeward_found/spec/models/sighting_spec.rb
@@ -5,4 +5,9 @@ describe Sighting do
     sighting = FactoryGirl.create(:sighting, :event_date => Time.new(2014, 7, 26, 12, 12, 0, "+00:00"))
     expect(sighting.long_date).to eq ("July 26th, 2014 12:12")
   end
+
+  it 'returns a maximum of 10 event dates upon call to ordered_json' do
+    15.times { FactoryGirl.create(:sighting) }
+    expect(JSON.parse(Sighting.ordered_json).length).to eq(10)
+  end
 end
